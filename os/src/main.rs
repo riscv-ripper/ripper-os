db92e1d018de06fe5148cfc500b48f63d20cde0c
hsqStephenZhang <2250015961@qq.com>
2021-12-03 02:02:19 +0000

    finish boot and print

--- os/src/main.rs
+++ os/src/main.rs
@@ -6,6 +6,7 @@
 #![feature(alloc_error_handler)]
 
 mod lang_items;
+mod console;
 
 #[allow(deprecated)]
 mod sbi;
@@ -16,24 +17,17 @@ global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 extern "C" fn rust_main() {
-    console_putchar(b'O');
-    console_putchar(b'K');
-    console_putchar(b'\n');
+    clear_bss();
+    println!("hello world");
     loop{};
 }
 
-pub fn console_putchar(ch: u8) {
-    let _ret: usize;
-    let arg0: usize = ch as usize;
-    let arg1: usize = 0;
-    let arg2: usize = 0;
-    let which: usize = 1;
-    unsafe {
-        llvm_asm!("ecall"
-             : "={x10}" (_ret)
-             : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
-             : "memory"
-             : "volatile"
-        );
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
     }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
 }
